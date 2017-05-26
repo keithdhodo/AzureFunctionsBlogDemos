f52293cf1c091e1aacbc8d7aa87203bf731fe90c
ssfcultra <koolkeith13@gmail.com>
2017-05-26 14:38:28 -0700

    Added QuickUnion algorithm to demo the walkthrough for blog.

---
+++ AzureFunctionsBlogDemos/QuickUnion/QuickUnion.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Newtonsoft.Json;
+using System;
+using System.Diagnostics;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace AzureFunctionsBlogDemos.Sorting
+{
+    public class QuickUnion
+    {
+        public static async Task<HttpResponseMessage> Run(HttpRequestMessage req, TraceWriter log,
+            IAsyncCollector<Arrays> outputTable)
+        {
+
+            log.Info("QuickUnionTrigger processed a request.");
+
+            // Parse request input
+            string jsonContent = await req.Content.ReadAsStringAsync();
+            var quickFind = JsonConvert.DeserializeObject<Arrays>(jsonContent);
+            log.Info($"Inputs: {quickFind.NumberToUnionFrom.ToString()}, {quickFind.NumberToUnionTo.ToString()}");
+
+            if (quickFind.NumberToUnionFrom.Length != quickFind.NumberToUnionTo.Length)
+            {
+                return req.CreateResponse(HttpStatusCode.OK, new
+                {
+                    message = "Number of items to union do not match."
+                });
+            }
+
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            Arrays.QuickUnion(quickFind);
+
+            stopwatch.Stop();
+            quickFind.Runtime = stopwatch.Elapsed;
+
+            quickFind.AlgorithmName = "QuickUnion";
+            quickFind.PartitionKey = "Sorting";
+            quickFind.RowKey = Guid.NewGuid().ToString();
+
+            await outputTable.AddAsync(quickFind);
+
+            return req.CreateResponse(HttpStatusCode.OK, new
+            {
+                message = JsonConvert.SerializeObject(quickFind)
+            });
+        }
+    }
+}
