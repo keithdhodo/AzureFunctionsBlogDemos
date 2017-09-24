f419a5e2e96f66beb60601a066055cb60426688d
ssfcultra <koolkeith13@gmail.com>
2017-09-24 15:26:16 -0700

    Added more unit tests for JosephusTests.cs #288

--- Chapter03.Tests/JosephusTests.cs
+++ Chapter03.Tests/JosephusTests.cs
@@ -1,16 +1,46 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using AlgorithmsFunctions.Shared.Chapter03;
+using System;
 
 namespace Chapter03.Tests
 {
     [TestClass]
     public class JosephusTests
     {
+
         [TestMethod]
         public void Josephus_TestNineParticipantsAndEliminateByFive()
         {
             var result = new Josephus(numberOfParticipants: 9, orderToRemoveParticipants: 5).ExecuteJosephusSimulation();
             Assert.AreEqual(8, result.Item);
+        }
+
+        [TestMethod]
+        public void Josephus_TestOneThousandParticipantsAndEliminateByTwo()
+        {
+            var result = new Josephus(numberOfParticipants: (int)Math.Pow(10, 3), orderToRemoveParticipants: 2).ExecuteJosephusSimulation();
+            Assert.AreEqual(999, result.Item);
+        }
+
+        [TestMethod]
+        public void Josephus_TestTenThousandParticipantsAndEliminateByThree()
+        {
+            var result = new Josephus(numberOfParticipants: (int)Math.Pow(10, 4), orderToRemoveParticipants: 2).ExecuteJosephusSimulation();
+            Assert.AreEqual(9999, result.Item);
+        }
+
+        [TestMethod]
+        public void Josephus_TestOneHundredThousandParticipantsAndEliminateByFive()
+        {
+            var result = new Josephus(numberOfParticipants: (int)Math.Pow(10, 5), orderToRemoveParticipants: 2).ExecuteJosephusSimulation();
+            Assert.AreEqual(99999, result.Item);
+        }
+
+        [TestMethod]
+        public void Josephus_TestOneMillionParticipantsAndEliminateByFive()
+        {
+            var result = new Josephus(numberOfParticipants: (int)Math.Pow(10, 6), orderToRemoveParticipants: 2).ExecuteJosephusSimulation();
+            Assert.AreEqual(999999, result.Item);
         }
     }
 }
