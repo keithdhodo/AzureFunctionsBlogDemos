5ec9045e12154da698c3e22c38dc7177c453c8c3
ssfcultra <koolkeith13@gmail.com>
2017-09-25 16:43:24 -0700

    Added another test for the Josephus problem and found the pattern. Intruiging. #290

--- Chapter03.Tests/JosephusTests.cs
+++ Chapter03.Tests/JosephusTests.cs
@@ -25,21 +25,28 @@ namespace Chapter03.Tests
         [TestMethod]
         public void Josephus_TestTenThousandParticipantsAndEliminateByThree()
         {
-            var result = new Josephus(numberOfParticipants: (int)Math.Pow(10, 4), orderToRemoveParticipants: 2).ExecuteJosephusSimulation();
+            var result = new Josephus(numberOfParticipants: (int)Math.Pow(10, 4), orderToRemoveParticipants: 3).ExecuteJosephusSimulation();
             Assert.AreEqual(9999, result.Item);
         }
 
         [TestMethod]
         public void Josephus_TestOneHundredThousandParticipantsAndEliminateByFive()
         {
-            var result = new Josephus(numberOfParticipants: (int)Math.Pow(10, 5), orderToRemoveParticipants: 2).ExecuteJosephusSimulation();
+            var result = new Josephus(numberOfParticipants: (int)Math.Pow(10, 5), orderToRemoveParticipants: 5).ExecuteJosephusSimulation();
             Assert.AreEqual(99999, result.Item);
         }
 
         [TestMethod]
-        public void Josephus_TestOneMillionParticipantsAndEliminateByFive()
+        public void Josephus_TestOneMillionParticipantsAndEliminateByTen()
         {
-            var result = new Josephus(numberOfParticipants: (int)Math.Pow(10, 6), orderToRemoveParticipants: 2).ExecuteJosephusSimulation();
+            var result = new Josephus(numberOfParticipants: (int)Math.Pow(10, 6), orderToRemoveParticipants: 10).ExecuteJosephusSimulation();
+            Assert.AreEqual(999999, result.Item);
+        }
+
+        [TestMethod]
+        public void Josephus_TestOneMillionParticipantsAndEliminateByFifty()
+        {
+            var result = new Josephus(numberOfParticipants: (int)Math.Pow(10, 6), orderToRemoveParticipants: 50).ExecuteJosephusSimulation();
             Assert.AreEqual(999999, result.Item);
         }
     }
