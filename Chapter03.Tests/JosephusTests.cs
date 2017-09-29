d7a58ac0d183667cabd74fb0f04cf95e137554b4
ssfcultra <koolkeith13@gmail.com>
2017-09-29 12:33:15 -0700

    Updated the tests for Josephus. The class isn't producting the right results, yet.

--- Chapter03.Tests/JosephusTests.cs
+++ Chapter03.Tests/JosephusTests.cs
@@ -7,54 +7,56 @@ namespace Chapter03.Tests
     [TestClass]
     public class JosephusTests
     {
+        // These are all totally wrong except for the second test...
+        // correct answers for the tests: http://webspace.ship.edu/deensley/mathdl/Joseph.html
 
         [TestMethod]
         public void Josephus_TestNineParticipantsAndEliminateByFive()
         {
             var result = new Josephus(numberOfParticipants: 9, orderToRemoveParticipants: 5).ExecuteJosephusSimulation();
-            Assert.AreEqual(8, result.Item);
+            Assert.AreEqual(3, result.Item);
         }
 
         [TestMethod]
         public void Josephus_TestOneThousandParticipantsAndEliminateByTwo()
         {
             var result = new Josephus(numberOfParticipants: (int)Math.Pow(10, 3), orderToRemoveParticipants: 2).ExecuteJosephusSimulation();
-            Assert.AreEqual(999, result.Item);
+            Assert.AreEqual(977, result.Item);
         }
 
         [TestMethod]
         public void Josephus_TestTenThousandParticipantsAndEliminateByThree()
         {
             var result = new Josephus(numberOfParticipants: (int)Math.Pow(10, 4), orderToRemoveParticipants: 3).ExecuteJosephusSimulation();
-            Assert.AreEqual(9999, result.Item);
+            Assert.AreEqual(3617, result.Item);
         }
 
         [TestMethod]
         public void Josephus_TestOneHundredThousandParticipantsAndEliminateByFive()
         {
             var result = new Josephus(numberOfParticipants: (int)Math.Pow(10, 5), orderToRemoveParticipants: 5).ExecuteJosephusSimulation();
-            Assert.AreEqual(99999, result.Item);
+            Assert.AreEqual(68929, result.Item);
         }
 
         [TestMethod]
         public void Josephus_TestOneMillionParticipantsAndEliminateByTen()
         {
             var result = new Josephus(numberOfParticipants: (int)Math.Pow(10, 6), orderToRemoveParticipants: 10).ExecuteJosephusSimulation();
-            Assert.AreEqual(999999, result.Item);
+            Assert.AreEqual(951425, result.Item);
         }
 
         [TestMethod]
         public void Josephus_TestOneMillionParticipantsAndEliminateByFifty()
         {
             var result = new Josephus(numberOfParticipants: (int)Math.Pow(10, 6), orderToRemoveParticipants: 50).ExecuteJosephusSimulation();
-            Assert.AreEqual(999999, result.Item);
+            Assert.AreEqual(951425, result.Item);
         }
 
         [TestMethod]
         public void Josephus_TestTenMillionParticipantsAndEliminateByFifty()
         {
             var result = new Josephus(numberOfParticipants: (int)Math.Pow(10, 7), orderToRemoveParticipants: 50).ExecuteJosephusSimulation();
-            Assert.AreEqual(9999999, result.Item);
+            Assert.AreEqual(3222785, result.Item);
         }
     }
 }
